afc61e63edd6279839cd4bc571900b9b34836230
Ren Nukariya <220c0338@s.obirn.ac.jp>
2021-07-18 22:43:47 +0900

    チャットが2回出るのをなおした

--- app.js
+++ app.js
@@ -20,12 +20,7 @@ io.on('connection',function(socket){
         messages=messages.slice(-100); // 最新の100件だけ保存
         io.emit('message', msg);
     });
-});
 
-io.on('connection',function(socket){
-    socket.on('message',function(msg){
-        io.emit('message', msg);
-    });
 });
 
 http.listen(PORT, function(){
